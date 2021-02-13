d4c35c868b83e30d85b3a8b137d9683befd0a5f0
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-02-13 13:57:12 -0800

    initial Block class construction

---
+++ index.js
@@ -0,0 +1,11 @@
+class Block {
+    constructor(data, hash, lastHash) {
+        this.data = data
+        this.hash = hash
+        this.lastHash = lastHash
+    }
+}
+
+const fooBlock = new Block('foo-data', 'foo-hash', 'foo-lastHash')
+
+console.log(fooBlock)
